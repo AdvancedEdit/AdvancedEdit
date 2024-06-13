bbc02a6ad845a50b14c7e4c046e262a9264971af
aplerdal <aplerdal01@gmail.com>
2024-06-13 12:43:53 -0500

    Rename

---
+++ AdvancedLib/Serialize/Ai.cs
@@ -0,0 +1,29 @@
+using BinarySerializer;
+
+namespace AdvancedLib.Serialize;
+
+public class Ai : BinarySerializable{
+    byte zonesCount {get; set;}
+    Pointer zonesPointer {get; set;}
+    Pointer targetsPointer {get; set;}
+    public override void SerializeImpl(SerializerObject s)
+    {
+        zonesCount = s.Serialize<byte>(
+            zonesCount,
+            nameof(zonesCount)
+        );
+        zonesPointer = s.SerializePointer(
+            zonesPointer,
+            PointerSize.Pointer8,
+            name:nameof(zonesPointer)
+        );
+
+        s.SerializePadding(1);
+
+        targetsPointer = s.SerializePointer(
+            targetsPointer,
+            PointerSize.Pointer16,
+            name:nameof(targetsPointer)
+        );
+    }
+}
